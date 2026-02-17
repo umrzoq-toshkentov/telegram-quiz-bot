b05d8ff2ca37a9cdfd33cf0354b183e29e39fa04
umrzoq-toshkentov <ssdumrzoq@gmail.com>
2026-02-17 05:24:18 +0500

    feat: updated with db path name

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { Database } from 'bun:sqlite';
 
-const db = new Database('quiz.db');
+const dbPath = process.env.DB_PATH || 'quiz.db';
+const db = new Database(dbPath);
 
 db.run('PRAGMA journal_mode = WAL');
 db.run(`
